99122e9214770052a9fc74ac082089380028486e
suyash <sharmasuyash0505@gmail.com>
2025-07-16 14:22:12 +0530

    lecture_8

---
+++ Lecture_8/index.js
@@ -0,0 +1,23 @@
+// var {sumOfTwo, xy} = require("./sumOfTwo");
+
+const {sumOfTwo, xy} = require("./sumOfTwo");
+
+function subtract(a,b){
+    console.log(a-b);
+    return a-b;
+}
+
+function multiply(a,b){
+    console.log(a*b);
+    return a*b;
+}
+
+function divide(a,b){
+    console.log(a/b);
+    return a/b;
+}
+
+var xy = 1000;
+// sumOfTwo(2,3);
+console.log("Xy:" , xy);
+// console.log(sumOfTwo(2,3))
---
+++ Lecture_8/sumOfTwo.js
@@ -0,0 +1,8 @@
+export function sumOfTwo(a,b){
+    console.log(a+b);
+    return a+b;
+}
+
+// var xy = 300;
+
+// module.exports = {sumOfTwo, xy};
